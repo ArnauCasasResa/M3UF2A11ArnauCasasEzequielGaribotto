5d280a182c47e95b935778cf3aeacb23920cca25
EzequielGaribotto <ezequiel.garibotto.7e7@itb.cat>
2024-01-11 08:53:11 +0100

    paintRegion with tests

--- src/Pintar una regio.kt
+++ src/Pintar una regio.kt
@@ -1,3 +1,41 @@
+import java.util.*
+
+fun paintRegion(array: Array<Array<Int>>, x: Int, y: Int): Array<Array<Int>> {
+    if (x < 0 || x >= array.size || y < 0 || y >= array[0].size || array[x][y] != 1) {
+        return array
+    }
+
+    array[x][y] = 2
+
+    paintRegion(array, x + 1, y)
+    paintRegion(array, x - 1, y)
+    paintRegion(array, x, y + 1)
+    paintRegion(array, x, y - 1)
+
+    return array
+}
+
 fun main() {
-    println("dejamee!")
+    val arrayInput = arrayOf(
+        arrayOf(0, 1, 1, 1, 0, 1, 0, 0),
+        arrayOf(0, 0, 1, 0, 0, 1, 0, 1),
+        arrayOf(0, 1, 1, 1, 0, 1, 0, 1),
+        arrayOf(0, 1, 1, 0, 0, 1, 0, 0),
+        arrayOf(0, 0, 1, 1, 0, 1, 1, 1),
+        arrayOf(0, 0, 0, 0, 0, 1, 0, 0),
+        arrayOf(1, 1, 1, 1, 1, 1, 1, 0),
+        arrayOf(0, 0, 0, 0, 0, 1, 0, 0)
+    )
+
+    val x = 3
+    val y = 3
+
+    val paintedArray = paintRegion(arrayInput, x-1, y-1)
+
+    for (i in paintedArray.indices) {
+        for (j in paintedArray[i].indices) {
+            print("${paintedArray[i][j]} ")
+        }
+        println()
+    }
 }
---
+++ test/Pintar_una_regioKtTest.kt
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Pintar_una_regioKtTest {
+
+    @Test
+    fun paintRegion() {
+        val arrayInput = arrayOf(
+            arrayOf(0, 1, 1, 1, 0, 1, 0, 0),
+            arrayOf(0, 0, 1, 0, 0, 1, 0, 1),
+            arrayOf(0, 1, 1, 1, 0, 1, 0, 1),
+            arrayOf(0, 1, 1, 0, 0, 1, 0, 0),
+            arrayOf(0, 0, 1, 1, 0, 1, 1, 1),
+            arrayOf(0, 0, 0, 0, 0, 1, 0, 0),
+            arrayOf(1, 1, 1, 1, 1, 1, 1, 0),
+            arrayOf(0, 0, 0, 0, 0, 1, 0, 0)
+        )
+
+        val x = 3
+        val y = 3
+
+        paintRegion(arrayInput,x-1,y-1)
+
+        val expectedArray = arrayOf(
+            arrayOf(0, 2, 2, 2, 0, 1, 0, 0),
+            arrayOf(0, 0, 2, 0, 0, 1, 0, 1),
+            arrayOf(0, 2, 2, 2, 0, 1, 0, 1),
+            arrayOf(0, 2, 2, 0, 0, 1, 0, 0),
+            arrayOf(0, 0, 2, 2, 0, 1, 1, 1),
+            arrayOf(0, 0, 0, 0, 0, 1, 0, 0),
+            arrayOf(1, 1, 1, 1, 1, 1, 1, 0),
+            arrayOf(0, 0, 0, 0, 0, 1, 0, 0))
+        assertArrayEquals(expectedArray,arrayInput)
+    }
+}
